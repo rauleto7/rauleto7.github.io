e6b7e91f780b84e544e268a637e1e1acf5328b2e
Raul <rauleto_7@hotmail.com>
2017-03-18 14:12:43 -0600

    avan1

--- Code/TodoBajoControl.java
+++ Code/TodoBajoControl.java
@@ -1,30 +1,16 @@
 import java.io.*;
 public class TodoBajoControl{
-
-
-	public static void main (String [] args){
-		InputStreamReader isr = new InputStreamReader(System.in);
-		BufferedReader bf = new BufferedReader(isr);
-		String dato;
-		PrintStream fsalida = System.out;
-		int op = 0;
-		int dato1 = 0;
-
-		fsalida.println("\tBienvido a la APP TodoBajoControl 'TBC' \n");
-		
-			fsalida.println("¿Qué opcion desea elegir\n");
-		fsalida.println("1.-Ingresar Datos\n.-2Revisar Datos\n3.-¿Como me protejo?");
-		
-		try{	
-			dato = bf.readLine();
-			op = Integer.parseInt(dato);
-		}catch(IOException ex) {}
+public static InputStreamReader isr = new InputStreamReader(System.in);
+public static BufferedReader bf = new BufferedReader(isr);
+public static PrintStream fsalida = System.out;
+public static String dato;
+public static int op, dato1;
 
 
 
-		switch(op){
-			case 1:
-			fsalida.println("Ingresar Datos");
+
+public static void datosPersonales(){
+	fsalida.println("Ingresar Datos");
 			try{
 				fsalida.println("Nombre");
 				dato = bf.readLine();
@@ -35,10 +21,41 @@ public class TodoBajoControl{
 				dato1 = Integer.parseInt(dato);
 
 			}catch(IOException ex){}
+		}
 
+
+
+	public static void main (String [] args){
+
+		do{
+		fsalida.println("\tBienvido a la APP TodoBajoControl 'TBC' \n");
+		
+			fsalida.println("¿Qué opcion desea elegir\n");
+		fsalida.println("1.-Ingresar Datos\n.-2Revisar Datos\n3.-¿Como me protejo?\n4.-Salir");
+		
+		try{	
+			dato = bf.readLine();
+			op = Integer.parseInt(dato);
+		}catch(IOException ex) {}
+
+
+
+		switch(op){
+			case 1:
+			datosPersonales();
+			break;
+			case 2:
 
 			break;
+			case 3:
+
+			break;
+			case 4:
+			fsalida.println("CUIDATE USA PROTECCION");
+			break;
 		}
+
+		}while(op != 4);
 
 	}
 	
